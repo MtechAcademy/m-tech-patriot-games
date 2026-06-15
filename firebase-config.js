229a298c9245c2915e31d2e2d9fd444ad542a287
MtechAcademy <charvey@a1garage.com>
2026-06-15 15:07:29 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,10 @@
 // STEP NEEDED: paste your Firebase config between the braces below.
 // It should look like: apiKey: "...", authDomain: "...", projectId: "...", etc.
 window.firebaseConfig = {
-  // PASTE FIREBASE CONFIG HERE
+  apiKey: "AIzaSyD4bv_9VRthbpMFwrQsFHTMPsH0STex_6w",
+  authDomain: "m-tech-challenge.firebaseapp.com",
+  projectId: "m-tech-challenge",
+  storageBucket: "m-tech-challenge.firebasestorage.app",
+  messagingSenderId: "1083681015209",
+  appId: "1:1083681015209:web:4fff749aee57e3cf7530b8"
 };
